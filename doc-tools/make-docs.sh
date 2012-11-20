94eceaba3f65f0be5ebbd344e52d6ed8bc2eebe5
Jacek Artymiak <jacek.artymiak@datasift.com>
2012-11-20 14:38:56 +0000

    Added /doc-tools/make-docs.sh

---
+++ doc-tools/make-docs.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+which docco >/dev/null 2>&1 || { echo >&2 \
+"This script requires additional software to run. If you run Ubuntu, please make sure that you install the following packages:\n" \
+"\n" \
+"sudo apt-get install nodejs\n" \
+"sudo apt-get install npm\n" \
+"sudo apt-get install python-setuptools\n" \
+"sudo npm install -g docco\n" \
+"sudo npm install -g coffee-script\n" \
+"sudo easy_install Pygments\n" \
+; exit 1; }
+
+docco ../*.js
+
+cd docs
+fl=`ls *html`
+cd ..
+
+echo -n '//' > index-body.js
+for f in $fl
+do 
+	echo -n '<a href="'$f'">'$f'</a><br />' >> index-body.js;
+done
+
+echo '<a href="https://github.com/datasift/NodeJS-Consumer/blob/master/LICENSE">LICENSE</a><br />' >> index-body.js
+echo '<a href="https://github.com/datasift/NodeJS-Consumer/blob/master/README.md">README.md</a><br />' >> index-body.js
+echo ' ' >> index-body.js
+
+cat index-header.js index-body.js index-footer.js > index.js
+docco index.js
+cat docs/index.html | sed -e 's/index.js/NodeJS-Consumer/g' > docs/index_new.html
+mv docs/index_new.html docs/index.html
+
+#rm -rf gh-pages
+#mv docs gh-pages
+#tar zcf gh-pages.tar.gz gh-pages
