894d60332e8600343745018f830bd6fd2e6f90d7
Jacek Artymiak <jacek.artymiak@datasift.com>
2012-12-27 15:25:08 +0000

    Changes to the documentation footer.

--- doc-tools/index-footer.js
+++ doc-tools/index-footer.js
@@ -2,3 +2,19 @@
 //The source code is available from <a href="https://github.com/datasift/NodeJS-Consumer">https://github.com/datasift/NodeJS-Consumer</a>
 
 //For more information about the DataSift API, visit the <a href="http://dev.datasift.com/docs">Documentation</a> site.
+
+//<h2>Source Code</h2>
+
+//<p>The DataSift NodeJS-Consumer Library is available for <a href='https://github.com/datasift/NodeJS-Consumer'>download</a>.</p>
+
+//<h2>Examples</h2>
+
+//See the <a href='http://datasift.github.com/NodeJS-Consumer/example.html'>examples</a> notes.
+
+//<h2>Documentation</h2>
+
+//The DataSift NodeJS-Consumer Library is used to access the DataSift platform via its Application Programming Interface. You can learn more about the DataSift API on our <a href="http://dev.datasift.com/docs">documentation</a> site.
+
+//<h2>Licensing</h2>
+
+//The DataSift NodeJS-Consumer Library is Open Source software. Please read the license.
